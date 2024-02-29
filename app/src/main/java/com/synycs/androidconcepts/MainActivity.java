a39a9432804f07ea5871e243f6ff1fbe2ebcc077
praveen <praveen.varaprasad@gmail.com>
2024-02-29 15:53:20 +0530

    changes

--- app/src/main/java/com/synycs/androidconcepts/MainActivity.java
+++ app/src/main/java/com/synycs/androidconcepts/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Update", Toast.LENGTH_SHORT).show();
 
         Log.i("Actuvity_LifeCycle","OnCreate");
 
